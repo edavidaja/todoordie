c88ac032a806b0a032191ccc5a06ecba39da39ba
edavidaja <david@rstudio.com>
2021-11-07 15:49:16 -0500

    use stringr, class tests

--- R/roxy-tag.R
+++ R/roxy-tag.R
@@ -6,21 +6,32 @@
 #' @export
 #'
 #' @importFrom roxygen2 roxy_tag_parse
+#' @importFrom stringr str_split str_extract
 roxy_tag_parse.roxy_tag_todoordie <- function(x) {
-  parsed <- strsplit(x$raw, "\n")
+  parsed <- str_split(x$raw, "\n")
 
   date_regex <- "[0-9]{4}-[0-9]{2}-[0-9]{2}"
-  matched_date <- regexpr(date_regex, parsed)
-  due_at <- regmatches(parsed, matched_date)
+  matched_date <- str_extract(parsed, date_regex)
 
-  first_word <- "^\\b[a-z_]+\\b"
-  matched_first_word <- regexpr(first_word, parsed)
-  condition <- regmatches(parsed, matched_first_word)
+  first_word_regex <- "^\\b[a-z_]+\\b"
+  matched_first_word <- str_extract(parsed, first_word_regex)
 
   x$val <- list(
-    condition = condition,
-    due_at = due_at,
-    note = parsed
+    # condition = condition,
+    due_at = matched_date,
+    note = matched_first_word
   )
   x
+}
+
+roclet_todoordie <- function() {
+  roclet("todoordie")
+}
+
+roclet_process.roclet_todoordie <- function(x, blocks, env, base_path) {
+
+}
+
+roclet_output.roclet_todoordie <- function(x, results, base_path, ...) {
+
 }
--- tests/testthat/test-roxy-tag.R
+++ tests/testthat/test-roxy-tag.R
@@ -3,12 +3,11 @@ sample_text <- "
   #'
   #' @todoordie after_date 2021-01-21 do after rstudio conf 2021
   #' @todoordie after_date 3000-12-31 way in the future
-  #' @md
   f <- function(x, y) {
-    # ...
+    NULL
   }
 "
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("tag parser works", {
+  expect_s3_class(roxygen2::parse_text(sample_text)[[1]], "roxy_block")
 })
